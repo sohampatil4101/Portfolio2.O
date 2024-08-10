f10442bed26d2af0bd74e76005112f9c952d9d41
Soham Patil <patilsoham390@gmail.com>
2024-08-11 00:55:49 +0530

    created navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import { ThemeProvider } from "styled-components"
 import {darkTheme} from "./React 3D Portfolio/utils/Themes"
 import { Navbar } from './components/Navbar';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
     <ThemeProvider theme = {darkTheme}>
-      <Navbar/>
+      <Router>
+        <Navbar/>
+      </Router>
     </ThemeProvider>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,69 @@
 import React from 'react'
-import Styled from "styled-components"
+import Styled, { styled } from "styled-components"
+import {Link as LinkR} from "react-router-dom"
 
-const NavbarContainer = Styled.div``
-const NavLogo = Styled.div``
+const NavbarContainer = Styled.div`
+  background-color: ${({theme}) => theme.bg};
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1rem;
+  position: sticky;
+  top:0;
+  z-index:10;
+  color: white
+`
+const NavLogo = Styled(LinkR)`
+  padding:0px 6px;
+  text-decoration: none;
+`
+
+const NavItems = Styled.ul`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 32px;
+  padding: 0px 6px;
+  list-style: none;
+`
+const NavLink = styled.a`
+  color: ${({theme}) => theme.text_primary};
+  font-weight: 500;
+  cursor: pointer;
+  transistion: all 0.2s ease-in-out;
+  text-decoration: none;
+  &:hover{
+    color: ${({theme}) => theme.primary};
+  }
+`
+
+const ButtonContainer = styled.div``
+const GithubButton = styled.div``
+
+
+
 
 export const Navbar = () => {
-  return (
+  return ( 
     <NavbarContainer>
-        <NavLogo><a href="">GFG</a></NavLogo>
+        <NavLogo to="/">soham</NavLogo>
+
+        <NavItems>
+          <NavLink href="#about">about</NavLink>
+          <NavLink href="#skills">skills</NavLink>
+          <NavLink href="#expirence">expirence</NavLink>
+          <NavLink href="#projects">projects</NavLink>
+          <NavLink href="#education">education</NavLink>
+        </NavItems>
+
+        <ButtonContainer>
+          <GithubButton>
+
+          </GithubButton>
+        </ButtonContainer>
+
     </NavbarContainer>
   )
 }
