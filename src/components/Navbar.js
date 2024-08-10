bc2eed9e5cd0d1fa4b4a899bb96b1a75f4aeee66
Soham Patil <patilsoham390@gmail.com>
2024-08-10 12:23:15 +0530

    created basic setup with dependencies installed

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Styled from "styled-components"
+
+const NavbarContainer = Styled.div``
+const NavLogo = Styled.div``
+
+export const Navbar = () => {
+  return (
+    <NavbarContainer>
+        <NavLogo><a href="">GFG</a></NavLogo>
+    </NavbarContainer>
+  )
+}
